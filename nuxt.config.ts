26b9ca514bafb0abbbd9d937ec9795dd9e3e49f8
Mohammad Barj Lazuardi Dindashwara <hashiramakun58@gmail.com>
2023-08-15 21:11:25 +0700

    feat: first commit

---
+++ nuxt.config.ts
@@ -0,0 +1,22 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  devtools: { enabled: true },
+  app: {
+    head: {
+      charset: 'utf-8',
+      viewport: 'width=device-width, initial-scale=1',
+    }
+  },
+  modules: [
+    '@nuxtjs/tailwindcss'
+  ],
+  tailwindcss: {
+    cssPath: '~/assets/css/tailwind.css',
+    configPath: 'tailwind.config',
+    exposeConfig: false,
+    exposeLevel: 2,
+    config: {},
+    injectPosition: 'first',
+    viewer: true,
+  }
+})
---
+++ pages/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <h1 class="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+  </div>
+</template>
+
+<script>
+export default {
+  setup() {
+    useHead({
+      title: 'Barjlazuardi | Portfolio',
+      meta: [
+        { name: 'description', content: 'My amazing portfolio site.' }
+      ],
+      bodyAttrs: {
+        class: 'test'
+      },
+      script: [ { innerHTML: 'console.log(\'Hello world\')' } ]
+    })
+  }
+}
+</script>
+
+<style></style>
